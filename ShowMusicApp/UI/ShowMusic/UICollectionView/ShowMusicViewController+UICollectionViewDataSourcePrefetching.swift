7f9db6b9bdce0c1e362404fcf99c0322c32a364d
Hieu Luong <hieu.luong.dinh.ext@proximus.com>
2021-05-11 16:12:14 +0200

    Prefetching: Todo

---
+++ ShowMusicApp/UI/ShowMusic/UICollectionView/ShowMusicViewController+UICollectionViewDataSourcePrefetching.swift
@@ -0,0 +1,19 @@
+//
+//  ShowMusicViewController+UICollectionViewDataSourcePrefetching.swift
+//  ShowMusicApp
+//
+//  Created by Hieu Luong on 5/1/21.
+//
+
+import UIKit
+
+extension ShowMusicViewController: UICollectionViewDataSourcePrefetching {
+    
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        // TODO
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+        // TODO
+    }    
+}
---
+++ ShowMusicApp/UI/ShowMusic/UITableView/ShowMusicViewController+UITableViewDataSourcePrefetching.swift
@@ -0,0 +1,19 @@
+//
+//  ShowMusicViewController+UITableViewDataSourcePrefetching.swift
+//  ShowMusicApp
+//
+//  Created by Hieu Luong on 5/1/21.
+//
+
+import UIKit
+
+extension ShowMusicViewController: UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        // TODO
+    }
+    
+    func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        // TODO
+    }
+}
